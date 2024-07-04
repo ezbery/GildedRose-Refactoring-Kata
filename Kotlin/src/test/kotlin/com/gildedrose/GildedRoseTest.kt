4db34d4a6450cc9b3135772f3d891165f2a54201
Ezbery <brenin0204@gmail.com>
2024-07-04 21:53:26 +0200

    base test + diff test

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -4,16 +4,34 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class GildedRoseTest {
-
     @Test
-    fun foo() {
+    fun `basic test`() {
+        //given
         val items = listOf(Item("foo", 0, 0))
         val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
 
+        assertEquals(items[0].name, app.items[0].name, "before: ${app.items[0]}")
+        assertEquals(items[0].sellIn, app.items[0].sellIn, "before: ${app.items[0]}")
+        assertEquals(items[0].quality, app.items[0].quality, "before: ${app.items[0]}")
+        //when
+        app.updateQuality()
+        //then
+        assertEquals(items[0].name, app.items[0].name, "after: ${app.items[0]}")
+        assertEquals(items[0].sellIn, app.items[0].sellIn, "after: ${app.items[0]}") //TODO -1
+        assertEquals(items[0].quality, app.items[0].quality, "after: ${app.items[0]}") //TODO -1
     }
 
+    @Test
+    fun diff() {
+        //given
+        val app = GildedRose(TestData.items)
+        val actual = mutableListOf(Constants.HEADER)
+        //when
+        for (i in 0..<Constants.DEFAULT_DAYS) {
+            actual.add(dayToString(i, app.items))
+            app.updateQuality()
+        }
+        //then
+        assertEquals(TestData.expectedOutput, actual.allDaysToString())
+    }
 }
-
-
